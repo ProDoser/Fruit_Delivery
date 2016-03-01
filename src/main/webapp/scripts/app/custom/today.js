6a0ed35437009da4b944e18a41d231f276e6a1d6
Leonid Zadorozhnykh <doser84@gmail.com>
2016-03-01 14:37:52 +0200

    custom view for today added (same as orders for now)

---
+++ src/main/webapp/scripts/app/custom/today.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Doser on 01.03.2016.
+ */
+
+'use strict';
+
+angular.module('fruitcrmApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('today', {
+                parent: 'entity',
+                url: '/today',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'fruitcrmApp.orders.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/custom/today.html',
+                        controller: 'OrdersController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('orders');
+                        $translatePartialLoader.addPart('global');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+
+    });
